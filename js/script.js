1ab90060619d9436deff9d149d933f60a129f46d
Ruslan Zoriichuk <rusikz94@gmail.com>
2020-05-06 19:20:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+cartButton.addEventListener('click', function(event) {
+    modal.classList.add('is-open');
+});
+
+close.addEventListener('click', function(event) {
+    modal.classList.remove('is-open');
+});
+
+// ! funtion toggleModal(){
+//     modal.classList.toggle('is-open');
+// }
+
+new WOW().init();
